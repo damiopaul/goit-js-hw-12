ff9bb320fd7c91d0ff0b95c2f939d7e138a4dd0a
damiopaul <damiopaul@gmail.com>
2024-04-05 23:55:24 +0300

    2

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,26 +1,5 @@
 
 import axios from "axios";
-// export function fetchImages(keyWord) {
-//     const apiOptions = new URLSearchParams ( {    
-//        key: "43133442-47d9d1fd9c6a7a8602dec5dde",
-//        image_type: "photo",
-//        orientation: "horizontal",
-//        safesearch: true,
-//        q: keyWord
-//    });
-
-//    const searchUrl = `https://pixabay.com/api/?${apiOptions}`;
-
-//    return fetch(searchUrl).then(response => {
-//     if (!response.ok){
-//         throw new Error (responce.status)
-//     }
-//     return response.json();
-//    });
-// }
-
-
-
 
 export async function fetchImages (keyWord, page, perPage) {
    
